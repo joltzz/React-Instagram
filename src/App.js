c41789682f8dd9fee8537a989bad9bdf7b3638b2
joltzz <guijsilva02@gmail.com>
2022-03-14 11:11:01 -0300

    erros finais/pronto p envio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./Components/Navbar";
 import Corpo from "./Components/Corpo";
-import FooterMobile from "./Components/FooterMobile";
+
 
 export default function App(){
     return (
